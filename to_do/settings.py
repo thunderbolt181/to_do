72d03406a22d2e2f15a5382d74d8f924d0abef73
Sachin Baghel <baghel.sachin47@gmail.com>
2021-08-07 12:55:10 +0530

    changing db to postgres

--- to_do/settings.py
+++ to_do/settings.py
@@ -67,7 +67,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "simple-todo.azurewebsites.net"
+    "https://simple-todo.azurewebsites.net",
+    'https://learningdjango.database.windows.net'
 ]
 
 REST_FRAMEWORK = {
@@ -102,9 +103,12 @@ WSGI_APPLICATION = 'to_do.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': os.getenv("DBUSER"),
+        'NAME': os.getenv("DBNAME"),
+        'PASSWORD':os.getenv("DBPASS"),
+        'HOST':os.getenv("DBHOST"),
+    },
 }
 
 
