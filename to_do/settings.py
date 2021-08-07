057317ee77fd7624fc04706c2a92b0850b6b1553
Sachin Baghel <baghel.sachin47@gmail.com>
2021-08-07 08:32:04 +0530

    adding CORS ORIGIN WHITELIST

--- to_do/settings.py
+++ to_do/settings.py
@@ -67,6 +67,7 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "https://simple-todo.azurewebsites.net"
 ]
 
 REST_FRAMEWORK = {
